c8f677304a7e39e7529f23c34c69454b16c55b51
Anna Pitera <annapitera101@gmail.com>
2024-01-19 23:50:48 -0500

    Converted specific columns in binary ints and prepared for one-hot encoding

--- mushrooms_final.R
+++ mushrooms_final.R
@@ -1,12 +1,26 @@
 library(tidyverse)
 library(ggthemes)
+library(caret) # for one-hot encoding categorical variables (the majority of variables are categorical in this dataset)
 library(dataMaid) # for auto EDA
 library(DataExplorer) # for auto EDA
 library(caTools) # for splitting data into training and test set
 
 # read in and view dataset - 8124 rows and 23 columns
-mushrooms <- read_csv('mushrooms.csv')
+mushrooms <- read_csv("mushrooms.csv")
 view(mushrooms)
+
+# convert boolean column ("bruises") into binary ints
+mushrooms$bruises <- as.integer(as.logical(mushrooms$bruises))
+view(mushrooms)
+
+# target column ("poisonous") is written as p = poisonous and e = edible
+# but it'd be better if it were just binary, with poisonous = 1 and edible = 0
+# convert target column ("poisonous") into binary ints
+mushrooms$poisonous <- factor(mushrooms$poisonous, levels=c("p", "e"), labels=c(1, 0))
+view(mushrooms)
+
+# one-hot encoding categorical variables using caret
+
 
 # drop target column (first column, labeled "poisonous")
 mushrooms <- subset(mushrooms, select = -c(poisonous))
@@ -30,4 +44,3 @@ sample <- sample.split(mushrooms$cap_shape, SplitRatio = 0.8)
 train <- subset(mushrooms, sample == TRUE)
 test <- subset(mushrooms, sample == FALSE)
 
-# 
