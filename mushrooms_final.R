42c868bb233f757b34b3a4b79027d75e15ec9641
Anna Pitera <annapitera101@gmail.com>
2024-01-19 23:45:18 -0500

    First day commmit (reading in data, auto EDA, splitting into training and test set)

---
+++ mushrooms_final.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(ggthemes)
+library(dataMaid) # for auto EDA
+library(DataExplorer) # for auto EDA
+library(caTools) # for splitting data into training and test set
+
+# read in and view dataset - 8124 rows and 23 columns
+mushrooms <- read_csv('mushrooms.csv')
+view(mushrooms)
+
+# drop target column (first column, labeled "poisonous")
+mushrooms <- subset(mushrooms, select = -c(poisonous))
+view(mushrooms)
+
+# auto EDA with dataMaid - outputs as html 
+makeDataReport(mushrooms, output = "html", replace = TRUE)
+
+# auto EDA with DataExplorer - outputs as html
+create_report(mushrooms)
+
+# for this particular dataset, auto EDA isn't super useful because of the way the dataset is set up
+# but leaving it in there just in case
+
+# set seed to ensure that split is the same every time file is run
+set.seed(72)
+
+# splitting data into training and test set with caTools
+# 80% train to 20% test ratio
+sample <- sample.split(mushrooms$cap_shape, SplitRatio = 0.8)
+train <- subset(mushrooms, sample == TRUE)
+test <- subset(mushrooms, sample == FALSE)
+
+# 
